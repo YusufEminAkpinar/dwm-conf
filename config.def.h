c97cda456a6c96b83d05efe4d42ef6986e1780fe
YusufEminAkpinar <yakpinar137@gmail.com>
2023-03-16 23:51:36 +0000

    Changing some settings

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -18,14 +18,18 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"monospace:size=10", "fontawesome:size=10", "NerdFonts:size=12"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
+static const char col_darkBlue[]    = "#1c1c1c";
+static const char col_black[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_yellow[]      = "#eff54e";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_yellow  },
 };
 
 /* tagging */
@@ -37,8 +41,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* { "Gimp",     NULL,       NULL,       0,            1,           -1 }, */
+	   { "Steam",    NULL,       NULL,       1 << 8,       0,           -1 },
+	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
 };
 
 /* layout(s) */
@@ -84,20 +89,17 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-// static const char *layoutmenu_cmd = "layoutmenu.sh";
 static const char *upvol[]   = { "amixer", "set", "Master", "5%+",     NULL };
 static const char *downvol[] = { "amixer", "set", "Master", "5%-",     NULL };
-static const char *mutevol[] = { "amixer", "set", "Master", "toggle",  NULL };
 
 #include "shiftview.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-  { MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("flameshot gui") },
-  { MODKEY,                       XK_Right,  spawn,          {.v = upvol   } },
+    { MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("flameshot gui") },
+    { MODKEY,                       XK_Right,  spawn,          {.v = upvol   } },
 	{ MODKEY,                       XK_Left,   spawn,          {.v = downvol } },
-	{ MODKEY,                       XK_End,    spawn,          {.v = mutevol } },
 	{ MODKEY,                       XK_p,      spawn,          SHCMD("rofi -show combi -modes combi -combi-modes 'drun,run'")},
-  { MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dmenucmd} },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dmenucmd} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
